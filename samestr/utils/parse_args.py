f720ccb2a30accf244d98a0c44bd01a3857ef63d
Christian Schudoma <christian.schudoma@embl.de>
2024-10-10 15:56:13 +0200

    add --random-seed parameter to allow reproducible filter/compare/extract runs

--- samestr/utils/parse_args.py
+++ samestr/utils/parse_args.py
@@ -49,6 +49,12 @@ def read_params():
         type=str,
         help='Set the verbosity of the program. '
              'Options: DEBUG, INFO, WARNING, ERROR, CRITICAL.')
+    
+    parser.add_argument(
+        '--random-seed',
+        type=int,
+        help='Set random seed for reproducible runs (affects compare, extract, filter.)'
+    )
 
     # add subparsers for different commands
     subparser = parser.add_subparsers(
