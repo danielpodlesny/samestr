21be717db5e7eb7d8f49ab3d0b1d49cc29cc629e
Christian Schudoma <christian.schudoma@embl.de>
2025-10-07 18:41:27 +0200

    made array-growing in freq2freqs more efficient

--- samestr/merge/freq2freqs.py
+++ samestr/merge/freq2freqs.py
@@ -7,63 +7,66 @@ from samestr.utils.utilities import load_numpy_file
 LOG = logging.getLogger(__name__)
 
 
-def merge_freqs(freqs, freq, freq_name):
-    """Appends two numpy arrays."""
+# def merge_freqs(freqs, freq, freq_name):
+#     """Appends two numpy arrays."""
 
-    if freqs.shape[1] == freq.shape[1]:
-        _freqs = np.append(freqs, freq, axis=0)
-        if _freqs.shape[0] == freqs.shape[0] + freq.shape[0]:
-            return _freqs, True
-        else:
-            LOG.error('Could not add array: %s' % freq_name)
-    else:
-        LOG.error('Dimensions of arrays do not match: %s (%s|%s)' %
-                  (freq_name, freqs.shape[1], freq.shape[1]))
+#     if freqs.shape[1] == freq.shape[1]:
+#         _freqs = np.append(freqs, freq, axis=0)
+#         if _freqs.shape[0] == freqs.shape[0] + freq.shape[0]:
+#             return _freqs, True
+#         else:
+#             LOG.error('Could not add array: %s' % freq_name)
+#     else:
+#         LOG.error('Dimensions of arrays do not match: %s (%s|%s)' %
+#                   (freq_name, freqs.shape[1], freq.shape[1]))
 
-    return freqs, False
-
+#     return freqs, False
 
 def freq2freqs(args):
     """Merges nucleotide frequencies for individual clades."""
 
     clade_freqs = None
-    samples = []
+    clade_samples = []
 
     # skip if clade file exists
     output_file = '%s/%s' % (args['output_dir'], args['clade'])
     if isfile('%s.npz' % output_file):
         LOG.info('Skipping: %s. Output file existed.' % args['clade'])
         return True
+    
+    if len(args['input_files']) > 1:
+        LOG.info(
+            'Merging %s inputs: %s' % (len(args['input_files']), args['clade'])
+        )
 
     # iterate samples of clade
-    for sample, file_path in args['input_files']:
+    for i, (sample, file_path) in enumerate(args['input_files']):
 
-        # initialize clade freqs arrays
+        sample_clade_freqs = load_numpy_file(file_path)
+
         if clade_freqs is None:
-            clade_freqs = load_numpy_file(file_path)
-            if isinstance(sample, list):
-                samples += sample
-            else:
-                samples.append(sample)
+            # final clade_freqs array will have dimensions (n_samples x clade_positions x 4)
+            clade_freqs = np.zeros(
+                (
+                    len(args['input_files']),      # n_samples
+                    sample_clade_freqs.shape[1],   # n_positions
+                    sample_clade_freqs.shape[2],   # n_variants   
+                )
+            )
 
-            # if more than one sample, continue samples loop
-            if len(args['input_files']) > 1:
-                LOG.info('Merging %s inputs: %s' %
-                         (len(args['input_files']), args['clade']))
-                continue
+        try:
+            clade_freqs[i] = sample_clade_freqs
+        except ValueError:
+            LOG.error(
+                'Dimensions of arrays do not match: %s (%s|%s)' %
+                (sample, sample_clade_freqs.shape, clade_freqs.shape)
+            )
+            return False
 
-        # if more than one sample, add sample freq to freqs
-        if len(args['input_files']) > 1:
-            clade_freqs, success = merge_freqs(
-                clade_freqs, load_numpy_file(file_path), sample)
-            if success:
-                if isinstance(sample, list):
-                    samples += sample
-                else:
-                    samples.append(sample)
+        clade_samples += (sample if isinstance(sample, list) else (sample,))
 
     # per clade, save freqs and names to file
     np.savez_compressed(output_file + '.npz', clade_freqs, allow_pickle=True)
 
-    with open(output_file + '.names.txt', 'w') as file:
-        file.write('\n'.join(samples) + '\n')
+    with open(output_file + '.names.txt', 'w') as _out:
+        print(*clade_samples, sep="\n", file=_out)
